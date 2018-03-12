b61aeffebdd85c13bad90df58fb40b8584528084
WORK\practitioner <baba7712@naver.com>
2018-03-12 16:51:39 +0900

    ejs page update// view reference by a-mean-blogger
    app.js port 3000 => 80 change

--- web/src/main/java/co/binoofactory/bmq/jeongwon/six/app.js
+++ web/src/main/java/co/binoofactory/bmq/jeongwon/six/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const morgan = require('morgan');
-const port     = process.env.PORT || 3000;
+const port     = process.env.PORT || 80;
 
 //config 정의
 
@@ -28,6 +28,8 @@ app.use(session({
 
 //라우터
 require('./routers/route.js')(app);
+
+
 //시작
 app.listen(port);
 console.log('using port : ' + port);
--- web/src/main/java/co/binoofactory/bmq/jeongwon/six/config/database.js
+++ web/src/main/java/co/binoofactory/bmq/jeongwon/six/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     host     : 'localhost',
     user     : 'root',
-    password : 'Akdxh213',
+    password : 'autoset',
     database : 'nodemysql'
 }
